c4dc7b10993c33857da51532689d468c0b121f7a
abdallah <sw.ouahmane@gmail>
2024-02-20 14:54:44 -0800

    task1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,41 +1,48 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers
- * @list: doubly linked list of integers
- *
- * Return: Void.
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
  */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
 
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *n, *h, *t;
+	listint_t *iter, *insert, *tmp;
 
-	if (!(*list) || !(*list)->next)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	n = *list;
-	while (n)
+
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		if (n->next && (n->n > n->next->n))
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			h = n->next;
-			for (t = h; t->prev; t = t->prev)
-				if (t->prev->n < t->n)
-					break;
-
-			if (h->next && h->prev)
-				h->prev->next = h->next, h->next->prev = h->prev;
-			else
-				h->prev->next = NULL;
-
-			h->next = t;
-			if (t->prev)
-				t->prev->next = h, h->prev = t->prev, t->prev = h;
-			else
-				t->prev = h, h->prev = NULL, *list = h;
-			print_list(*list), n = *list;
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-		else
-			n = n->next;
 	}
 }
